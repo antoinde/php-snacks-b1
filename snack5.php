692264c8c7c92d0b6fc9f2e81c5a07a39b5dc77c
antoinde <antonello.indelicato98@gmail.com>
2022-10-14 13:12:16 +0200

    Snack 5 svolto

--- snack5.php
+++ snack5.php
@@ -9,4 +9,33 @@ Creare un array contenente qualche alunno di un’ipotetica classe. Ogni alunno 
 
  */
 
+$classe = [
+    [
+        'name' => 'Mario',
+        'lastname' => 'Rossi',
+        'votes' => [9,9,9,9,9,9,9,10]
+    ],
+    [
+        'name' => 'Bartolo',
+        'lastname' => 'Meo',
+        'votes' => [5,6,7,8,9]
+    ],
+    [
+        'name' => 'Tina',
+        'lastname' => 'Ballerina',
+        'votes' => [2,3,4,5,6]
+    ]
+];
+
+for($i=0; $i < count($classe); $i++){
+    $student = $classe[$i];
+    $average = array_sum($student['votes']) / count($student['votes']);
+    ?>
+    <div>
+        <h2><?= $student['name'] ?> <?= $student['lastname'] ?></h2>
+        <p><?= round($average, 2) ?></p>
+    </div>
+    <?php
+}
+
  ?>
