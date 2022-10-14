b582d4cc8e9953a4d345552e0eb8e412b20c7d7f
antoinde <antonello.indelicato98@gmail.com>
2022-10-14 13:09:26 +0200

    Snack 4 completato

--- snack4.php
+++ snack4.php
@@ -8,4 +8,12 @@ Prendere un paragrafo abbastanza lungo, contenente diverse frasi. Prendere il pa
 
  */
 
+    $paragraph = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+    Pariatur distinctio reprehenderit provident nisi fugit accusamus quo. 
+    Quasi dicta natus. perferendis esse laboriosam quibusdam itaque. soluta aut. 
+    fugiat officia repudiandae error.';
+
+    $array = explode('.', $paragraph);
+    
+    var_dump($array);
  ?>
