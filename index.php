08b8aae638109d2140af65318760d3631c315545
antoinde <antonello.indelicato98@gmail.com>
2022-10-14 10:19:06 +0200

    Sistemati snacks, ordinati

--- index.php
+++ index.php
@@ -6,27 +6,6 @@ Ogni array avrà una squadra di casa e una squadra ospite,
 punti fatti dalla squadra di casa e punti fatti dalla squadra ospite. 
 Stampiamo a schermo tutte le partite con questo schema.
 Olimpia Milano - Cantù | 55-60
-
-Snack 2
-Passare come parametri GET name, mail e age e verificare 
-(cercando i metodi che non conosciamo nella documentazione) che name sia più lungo di
- 3 caratteri, che mail contenga un punto e una chiocciola e che age sia un numero. 
- Se tutto è ok stampare “Accesso riuscito”, altrimenti “Accesso negato”
-
-Snack 3
-Creare un array di array. Ogni array figlio avrà come chiave una data in questo 
-formato: DD-MM-YYYY es 01-01-2007 e come valore un array di post associati a quella data. 
-Stampare ogni data con i relativi post.
-Qui l’array di esempio: https://www.codepile.net/pile/R2K5d68z
-
-Snack 4
-Prendere un paragrafo abbastanza lungo, contenente diverse frasi. Prendere il paragrafo
- e suddividerlo in tanti paragrafi. Ogni punto un nuovo paragrafo.
-
-Snack 5
-Creare un array contenente qualche alunno di un’ipotetica classe. Ogni alunno avrà Nome,
- Cognome e un array contenente i suoi voti scolastici. Stampare Nome, Cognome 
- e la media dei voti di ogni alunno.
 */
 
 $partite= [
@@ -52,7 +31,7 @@ $partite= [
     
     
 ?>
-<h2>Partite: </h2>
+<h2>Snack 1: </h2>
 
 <?php
     for($i=0; $i < count($partite) ; $i++) {
@@ -67,3 +46,56 @@ $partite= [
         echo '</p>';
     }
 ?>
+
+<?php
+
+/* 
+------------------------------------------------------------------------------------------------
+Snack 2
+Passare come parametri GET name, mail e age e verificare 
+(cercando i metodi che non conosciamo nella documentazione) che name sia più lungo di
+ 3 caratteri, che mail contenga un punto e una chiocciola e che age sia un numero. 
+ Se tutto è ok stampare “Accesso riuscito”, altrimenti “Accesso negato”
+
+ */
+
+ ?>
+
+<?php
+
+/* 
+------------------------------------------------------------------------------------------------
+Snack 3
+Creare un array di array. Ogni array figlio avrà come chiave una data in questo 
+formato: DD-MM-YYYY es 01-01-2007 e come valore un array di post associati a quella data. 
+Stampare ogni data con i relativi post.
+Qui l’array di esempio: https://www.codepile.net/pile/R2K5d68z
+
+ */
+
+ ?>
+
+<?php
+
+/* 
+------------------------------------------------------------------------------------------------
+Snack 4
+Prendere un paragrafo abbastanza lungo, contenente diverse frasi. Prendere il paragrafo
+ e suddividerlo in tanti paragrafi. Ogni punto un nuovo paragrafo.
+
+ */
+
+ ?>
+
+<?php
+
+/* 
+------------------------------------------------------------------------------------------------
+Snack 5
+Creare un array contenente qualche alunno di un’ipotetica classe. Ogni alunno avrà Nome,
+ Cognome e un array contenente i suoi voti scolastici. Stampare Nome, Cognome 
+ e la media dei voti di ogni alunno.
+
+ */
+
+ ?>
